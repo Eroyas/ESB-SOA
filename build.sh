6245b34d1d2bd0c8dbe4d9a7d12cd55768a16698
Maxime Carlier <maxime.mca@gmail.com>
2017-10-01 20:14:23 +0200

    Update build script

--- build.sh
+++ build.sh
@@ -1,3 +1,40 @@
-#!/usr/bin/bash
-
+#!/bin/bash
 # Script triggering the building process
+
+PATH_TO_SCRIPT=${0%/*}
+
+# Check if the path that called the script is the same as the directory containing the script
+if [ "$PATH_TO_SCRIPT" != "." ]; then
+    cd "$PATH_TO_SCRIPT"
+    echo "Changing directory to $PATH_TO_SCRIPT"
+fi
+
+# Build maven project to generate web archive
+echo -n "Building maven project..."
+mvn -q clean package
+
+# Check for errors on last call
+if [ "$?" = "0" ]; then
+    echo "OK"
+else
+    echo "FAILURE"
+    exit 1
+fi
+
+set -e
+cd deployment
+set +e
+
+# Execute Docker Compose
+echo -n "Composing Docker images..."
+docker-compose up -d
+
+if [ "$?" = "0" ]; then
+    echo "OK"
+else
+    echo "FAILURE"
+    exit 1
+fi
+
+# Change dir to location before script launch
+cd "$OLDPWD"
