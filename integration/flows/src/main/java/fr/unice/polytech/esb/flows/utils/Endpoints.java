b614ee33a0efe5d47ffb035df2341970d72ee4f5
Khadim <khadim.gning@yahoo.fr>
2017-10-15 22:24:19 +0200

    Flight search flow WIP

--- integration/flows/src/main/java/fr/unice/polytech/esb/flows/utils/Endpoints.java
+++ integration/flows/src/main/java/fr/unice/polytech/esb/flows/utils/Endpoints.java
@@ -7,7 +7,9 @@ public class Endpoints {
 
     // Internal message queues
     public static final String CAR_RESERVATION_Q = "activemq:car-reservation";
+    public static final String FLIGHT_RESERVATION_Q = "activemq:flight-reservation";
 
     // External partners
     public static final String CAR_RESERVATION = "http://localhost:8080/tcs-cars-service/ExternalCarRentalService";
+    public static final String FLIGHT_RESERVATION = "http://localhost:8080/tcs-service-document/registry";
 }
