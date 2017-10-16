bc21adfe428d8bf7de7754f505642e69b4bb332f
GNINGK <khadim.gning@yahoo.fr>
2017-10-16 13:59:04 +0200

    Flight search flow fix

--- integration/flows/src/main/java/fr/unice/polytech/esb/flows/CallExternalPartners.java
+++ integration/flows/src/main/java/fr/unice/polytech/esb/flows/CallExternalPartners.java
@@ -41,43 +41,4 @@ public class CallExternalPartners extends RouteBuilder {
         exc.getIn().setBody(result);
     };
 
-    /*************************************************************************
-     ** Flight reservation (a Document service implemented using REST/JSON) **
-     *************************************************************************/
-
-    from(FLIGHT_RESERVATION_Q)
-                .routeId("flight-reservation-call")
-                .routeDescription("Retrieve flight available depending of user criteria")
-
-                .setProperty("res-uuid", simple("${body}"))
-
-                .setProperty("origin", simple("${body.origin}"))
-                .setProperty("destination", simple("${body.destination}"))
-                .setProperty("date" : simple("${body.date}")) //formatted as year-month-day, e.g. 2017-01-21)
-                .setProperty("timeFrom"	: simple("${body.timeFrom}")) //(formatted as hour-minute-second, e.g. 08-30-00)
-                .setProperty("timeTo"	: simple("${body.timeTo}"))
-                .setProperty("journeyType" : simple("${body.timeTo}")) // (DIRECT or INDIRECT)
-                .setProperty("maxTravelTime" : simple("${body.maxTravelTime}"))
-                .setProperty("category" : simple("${body.category}")) // (can be ECO, ECO_PREMIUM, BUSINESS, FIRST)
-                .setProperty("airline" : simple("${body.airline}"))
-            .log("Creating retrieval request for flights #${exchangeProperty[res-uuid]}")
-                .setHeader(Exchange.HTTP_METHOD, constant("POST"))
-            .setHeader("Content-Type", constant("application/json"))
-            .setHeader("Accept", constant("application/json"))
-            .process((Exchange exchange) -> {
-                String request = "{\n" +
-                        "  \"origin\": \""+ exchange.getProperty("origin", String.class) +"\",\n" +
-                        "  \"destination\": \""+ exchange.getProperty("destination", String.class) +"\",\n" +
-                        "  \"date\": \""+ exchange.getProperty("date", String.class) +"\",\n" +
-                        "  \"timeFrom\": \""+ exchange.getProperty("timeFrom", String.class) +"\",\n" +
-                        "  \"timeTo\": \""+ exchange.getProperty("timeTo", String.class) +"\",\n" +
-                        "  \"journeyType\": \"Direct\"\n" +
-                        "  \"maxTravelTime\": \""+ exchange.getProperty("maxTravelTime", Integer.class) +"\",\n" +
-                        "  \"category\": \"FIRST\",\n" +
-                        "  \"airline\": \""+ exchange.getProperty("airline", String.class) +"\"\n" +
-                        "}";
-                exchange.getIn().setBody(request);
-            })
-            .inOut(FLIGHT_RESERVATION)
-            ;
 }
---
+++ integration/flows/src/main/java/fr/unice/polytech/esb/flows/FlightExternalPartners.java
@@ -0,0 +1,61 @@
+package fr.unice.polytech.esb.flows;
+
+import fr.unice.polytech.esb.flows.data.CarInfo;
+import fr.unice.polytech.esb.flows.utils.CarReservationHelper;
+import static fr.unice.polytech.esb.flows.utils.Endpoints.*;
+
+import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.Processor;
+import org.apache.camel.Exchange;
+
+import javax.xml.transform.Source;
+import javax.xml.xpath.XPath;
+import javax.xml.xpath.XPathFactory;
+
+/**
+ * Created by GNINGK on 16/10/17.
+ */
+public class FlightExternalPartners extends RouteBuilder {
+
+    @Override
+    public void configure() throws Exception {
+        /*************************************************************************
+         ** Flight reservation (a Document service implemented using REST/JSON) **
+         *************************************************************************/
+
+        from(FLIGHT_RESERVATION_Q)
+                .routeId("flight-reservation-call")
+                .routeDescription("Retrieve flight available depending of user criteria")
+
+                .setProperty("res-uuid", simple("${body}"))
+
+                .setProperty("origin", simple("${body.origin}"))
+                .setProperty("destination", simple("${body.destination}"))
+                .setProperty("date", simple("${body.date}")) //formatted as year-month-day, e.g. 2017-01-21)
+                .setProperty("timeFrom", simple("${body.timeFrom}"))
+                .setProperty("timeTo", simple("${body.timeTo}"))
+                .setProperty("journeyType", simple("${body.timeTo}")) // (DIRECT or INDIRECT)
+                .setProperty("maxTravelTime", simple("${body.maxTravelTime}"))
+                .setProperty("category", simple("${body.category}")) // (can be ECO, ECO_PREMIUM, BUSINESS, FIRST)
+                .setProperty("airline", simple("${body.airline}"))
+                .log("Creating retrieval request for flights #${exchangeProperty[res-uuid]}")
+                .setHeader(Exchange.HTTP_METHOD, constant("POST"))
+                .setHeader("Content-Type", constant("application/json"))
+                .setHeader("Accept", constant("application/json"))
+                .process((Exchange exchange) -> {
+                    String request = "{\n" +
+                            "  \"origin\": \"" + exchange.getProperty("origin", String.class) + "\",\n" +
+                            "  \"destination\": \"" + exchange.getProperty("destination", String.class) + "\",\n" +
+                            "  \"date\": \"" + exchange.getProperty("date", String.class) + "\",\n" +
+                            "  \"timeFrom\": \"" + exchange.getProperty("timeFrom", String.class) + "\",\n" +
+                            "  \"timeTo\": \"" + exchange.getProperty("timeTo", String.class) + "\",\n" +
+                            "  \"journeyType\": \"Direct\"\n" +
+                            "  \"maxTravelTime\": \"" + exchange.getProperty("maxTravelTime", Integer.class) + "\",\n" +
+                            "  \"category\": \"FIRST\",\n" +
+                            "  \"airline\": \"" + exchange.getProperty("airline", String.class) + "\"\n" +
+                            "}";
+                    exchange.getIn().setBody(request);
+                })
+                .inOut(FLIGHT_RESERVATION)
+        ;
+    }
