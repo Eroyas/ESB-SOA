d3418076bc2329be047ed0a25db8e74ba8d0b8c2
Yasin EROGLU <yasin.eroglu@etu.unice.fr>
2017-10-23 02:21:14 +0200

    Car service : [json version] and [filtered version]

--- integration/flows/src/main/java/fr/unice/polytech/esb/flows/CallCarExternalPartners.java
+++ integration/flows/src/main/java/fr/unice/polytech/esb/flows/CallCarExternalPartners.java
@@ -1,16 +1,15 @@
 package fr.unice.polytech.esb.flows;
 
-import fr.unice.polytech.esb.flows.data.CarInfo;
 import fr.unice.polytech.esb.flows.utils.CarReservationHelper;
 import static fr.unice.polytech.esb.flows.utils.Endpoints.*;
 
 import org.apache.camel.builder.RouteBuilder;
 import org.apache.camel.Processor;
 import org.apache.camel.Exchange;
-
-import javax.xml.transform.Source;
-import javax.xml.xpath.XPath;
-import javax.xml.xpath.XPathFactory;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.json.XML;
 
 /**
  * Created by Eroyas on 14/10/17.
@@ -31,14 +30,46 @@ public class CallCarExternalPartners extends RouteBuilder {
                 .bean(CarReservationHelper.class, "buildRequest(${body}, ${header[duration]})")
 
                 .inOut(CAR_RESERVATION)
-                //.process(result2carInfo)
+                .process(result2carJson)
+                .process(result2filteredCarByPrice)
         ;
     }
 
-    private static Processor result2carInfo = (Exchange exc) -> {
-        XPath xpath = XPathFactory.newInstance().newXPath();
-        Source response = (Source) exc.getIn().getBody();
-        CarInfo result = new CarInfo(exc.getProperty("partial-car-info", CarInfo.class));
+    private static Processor result2carJson = (Exchange exc) -> {
+        Object response = exc.getIn().getBody();
+
+        try {
+            JSONObject jObj = XML.toJSONObject(String.valueOf(response));
+            String result = jObj.toString(4);
+
+            exc.getIn().setBody(result);
+        } catch (JSONException e) {
+            System.out.println(e.toString());
+        }
+    };
+
+    private static Processor result2filteredCarByPrice = (Exchange exc) -> {
+        Object response = exc.getIn().getBody();
+
+        JSONObject jObj = new JSONObject(response.toString());
+        JSONObject obj = jObj.getJSONObject("ns2:getCarRentalListResponse");
+        JSONArray rentals = (JSONArray) obj.get("car_rentals");
+
+        int bestPrice = (int) rentals.getJSONObject(0).get("rentPricePerDay");
+        int index = 0;
+
+        for (int i = 0; i < rentals.length(); i++) {
+
+            int price = (int) rentals.getJSONObject(i).get("rentPricePerDay");
+
+            if (bestPrice > price) {
+                bestPrice = price;
+                index = i;
+            }
+        }
+
+        String result = rentals.getJSONObject(index).toString(4);
+
         exc.getIn().setBody(result);
     };
 
--- integration/flows/src/test/java/fr/unice/polytech/esb/flows/CallCarExternalPartnersTest.java
+++ integration/flows/src/test/java/fr/unice/polytech/esb/flows/CallCarExternalPartnersTest.java
@@ -5,11 +5,6 @@ import static fr.unice.polytech.esb.flows.utils.Endpoints.*;
 import org.apache.camel.Exchange;
 import org.junit.Before;
 import org.junit.Test;
-import org.xml.sax.InputSource;
-
-import javax.xml.xpath.XPathConstants;
-import java.io.StringReader;
-import java.util.concurrent.TimeUnit;
 
 /**
  * Created by Eroyas on 15/10/17.
@@ -74,32 +69,75 @@ public class CallCarExternalPartnersTest extends ActiveMQTest {
                 "duration", "2",
                 String.class);
 
-/*
-        // ensuring assertions
+/*  Version 3 : local test and with json converter and filter */
+
         String expectedResponse = "" +
-                "<ns2:getCarRentalListResponse xmlns:ns2=\"http://informatique.polytech.unice.fr/soa/\">\n" +
-                "    <car_rentals>\n" +
-                "        <availability>2</availability>\n" +
-                "        <brand>MINI</brand>\n" +
-                "        <model>Cooper</model>\n" +
-                "        <place>France</place>\n" +
-                "        <rentPricePerDay>34</rentPricePerDay>\n" +
-                "    </car_rentals>\n" +
-                "    <car_rentals>\n" +
-                "        <availability>10</availability>\n" +
-                "        <brand>Lincoln</brand>\n" +
-                "        <model>Zephyr</model>\n" +
-                "        <place>France</place>\n" +
-                "        <rentPricePerDay>254</rentPricePerDay>\n" +
-                "    </car_rentals>\n" +
-                "</ns2:getCarRentalListResponse>";
+                "{\n" +
+                "    \"model\": \"Cooper\",\n" +
+                "    \"availability\": 2,\n" +
+                "    \"place\": \"France\",\n" +
+                "    \"brand\": \"MINI\",\n" +
+                "    \"rentPricePerDay\": 34\n" +
+                "}";
 
         assertEquals(expectedResponse, out);
-*/
 
+/*
         assertMockEndpointsSatisfied(1, TimeUnit.SECONDS);
         InputSource src =  new InputSource(new StringReader(out));
         assertTrue((boolean) xpath.evaluate("/", src, XPathConstants.BOOLEAN));
+*/
+
     }
 
 }
+
+/* Version 1 : local test and without json converter
+
+    String expectedResponse = "" +
+            "<ns2:getCarRentalListResponse xmlns:ns2=\"http://informatique.polytech.unice.fr/soa/\">\n" +
+            "    <car_rentals>\n" +
+            "        <availability>2</availability>\n" +
+            "        <brand>MINI</brand>\n" +
+            "        <model>Cooper</model>\n" +
+            "        <place>France</place>\n" +
+            "        <rentPricePerDay>34</rentPricePerDay>\n" +
+            "    </car_rentals>\n" +
+            "    <car_rentals>\n" +
+            "        <availability>10</availability>\n" +
+            "        <brand>Lincoln</brand>\n" +
+            "        <model>Zephyr</model>\n" +
+            "        <place>France</place>\n" +
+            "        <rentPricePerDay>254</rentPricePerDay>\n" +
+            "    </car_rentals>\n" +
+            "</ns2:getCarRentalListResponse>";
+
+    assertEquals(expectedResponse, out);
+*/
+
+/* Version 2 local test and with json converter
+
+    String expectedResponse = "" +
+        "{\"ns2:getCarRentalListResponse\": {\n" +
+        "    \"xmlns:ns2\": \"http://informatique.polytech.unice.fr/soa/\",\n" +
+        "    \"car_rentals\": [\n" +
+        "        {\n" +
+        "            \"model\": \"Cooper\",\n" +
+        "            \"availability\": 2,\n" +
+        "            \"place\": \"France\",\n" +
+        "            \"brand\": \"MINI\",\n" +
+        "            \"rentPricePerDay\": 34\n" +
+        "        },\n" +
+        "        {\n" +
+        "            \"model\": \"Zephyr\",\n" +
+        "            \"availability\": 10,\n" +
+        "            \"place\": \"France\",\n" +
+        "            \"brand\": \"Lincoln\",\n" +
+        "            \"rentPricePerDay\": 254\n" +
+        "        }\n" +
+        "    ]\n" +
+        "}}";
+
+    assertEquals(expectedResponse, out);
+
+*/
