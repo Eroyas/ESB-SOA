89f96bf251067f056a3b33b57065fcad3a3d8a20
Yasin EROGLU <yasin.eroglu@etu.unice.fr>
2017-11-10 22:58:04 +0100

    Model entrée dans message generator up

--- integration/flows/src/main/java/fr/unice/polytech/esb/flows/utils/TravelMessageGenerator.java
+++ integration/flows/src/main/java/fr/unice/polytech/esb/flows/utils/TravelMessageGenerator.java
@@ -10,7 +10,7 @@ public class TravelMessageGenerator {
     public String buildRequest(JSONObject json) {
         StringBuilder builder = new StringBuilder();
 
-        builder.append("TODO : IF FAUT : PaysDep PayArr DateDep DateArr");
+        builder.append("TODO : IF FAUT : PaysDep PayArr DateDep DateArr Customer(Id, firstname, lastname, email");
         builder.append("");
 
         return builder.toString();
